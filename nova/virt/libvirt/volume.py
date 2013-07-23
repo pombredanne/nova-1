fe2a8d643c97edc0ca5935d69ef646dfcda11036
Eric Harney <eharney@redhat.com>
2013-07-23 12:01:38 -0400

    Allow Cinder to specify file format for NFS/GlusterFS
    These drivers now have the option of passing a QCOW2 file rather
    than a raw file as the volume to attach to the VM.
    
    Implements blueprint qemu-assisted-snapshots
    
    Change-Id: I23929538c7dad65035db5c38ff6e52348925fa27

--- nova/virt/libvirt/volume.py
+++ nova/virt/libvirt/volume.py
@@ -486,6 +486,7 @@ class LibvirtNFSVolumeDriver(LibvirtBaseVolumeDriver):
         path = os.path.join(path, connection_info['data']['name'])
         conf.source_type = 'file'
         conf.source_path = path
+        conf.driver_format = connection_info['data'].get('format', 'raw')
         return conf
 
     def _ensure_mounted(self, nfs_export, options=None):
@@ -608,6 +609,7 @@ class LibvirtGlusterfsVolumeDriver(LibvirtBaseVolumeDriver):
         path = os.path.join(path, connection_info['data']['name'])
         conf.source_type = 'file'
         conf.source_path = path
+        conf.driver_format = connection_info['data'].get('format', 'raw')
         return conf
 
     def _ensure_mounted(self, glusterfs_export, options=None):
